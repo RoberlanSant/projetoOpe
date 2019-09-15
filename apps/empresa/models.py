7ad98fd91f8516d94cebad73438562302dab5fcc
RoberlanSant <roberfalc@gmail.com>
2019-09-15 06:52:59 -0300

    Criando relacionamentos de DADOS

--- apps/empresa/models.py
+++ apps/empresa/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
+
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text='nome da empresa')
+
+    def __str__(Self):
+        return self.nome

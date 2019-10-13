8d1d86e41abeb611e311d8e1c15f0bafb01f83f4
Roberlan Almeida Santiago <roberfalc@gmail>
2019-10-13 18:32:44 -0300

    corrigindo static

--- projetoOpe/settings.py
+++ projetoOpe/settings.py
@@ -101,9 +101,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "staticfiles"),
+#]
 
 MEDIA_URL = '/media/'
 

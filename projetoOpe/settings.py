94b439bd48a096abbf0c366e29603bff225fa370
Roberlan Almeida Santiago <roberfalc@gmail>
2019-10-13 04:48:46 -0300

    atualização para conecxão com servidor Amazon

--- projetoOpe/settings.py
+++ projetoOpe/settings.py
@@ -102,14 +102,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 LOGIN_REDIRECT_URL = 'home'
 

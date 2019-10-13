703ad46bc3297bfd0707d04314b60ce8893696e7
Roberlan Almeida Santiago <roberfalc@gmail>
2019-10-13 15:34:06 -0300

    atualizando acesso maquina virtual

--- projetoOpe/settings.py
+++ projetoOpe/settings.py
@@ -109,7 +109,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 LOGIN_REDIRECT_URL = 'home'
 

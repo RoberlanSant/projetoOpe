c4122d43c2623f2bafd145fcc4c1e5bde7be68f3
Roberlan Almeida Santiago <roberfalc@gmail>
2019-10-14 04:21:00 -0300

    Atualizando

--- projetoOpe/settings.py
+++ projetoOpe/settings.py
@@ -101,13 +101,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "staticfiles"),
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+#MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
